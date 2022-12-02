a03cac7d07c9d218ce27fa77e3f6916225ad7e92
nicolaslag2022 <nick.goforth@gmail.com>
2022-12-01 21:07:12 -0600

    spa 2

--- components/views/index.js
+++ components/views/index.js
@@ -2,4 +2,3 @@ export { default as Home } from "./Home";
 export { default as Bio } from "./Bio";
 export { default as Order } from "./Order";
 export { default as Pizza } from "./Pizza";
-export { default as Links } from "./Links";
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
-
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+
 const router = new Navigo("/");
 
 function render(state = store.Home) {
   document.querySelector("#root").innerHTML = `
-    ${Header(state)}
-    ${Nav(store.links)}
-    ${Main(state)}
-    ${Footer()}
-    `;
+  ${Header(state)}
+  ${Nav(store.Links)}
+  ${Main(state)}
+  ${Footer()}
+  `;
   router.updatePageLinks();
 }
 
 // add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+// document.querySelector(".fa-bars").addEventListener("click", () => {
+//   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+// });
 
 router
   .on({
