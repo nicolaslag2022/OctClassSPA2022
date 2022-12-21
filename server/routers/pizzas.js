69e9c3f2b5a6b5d2a46707cf8b5171c9167e3731
nicolaslag2022 <nick.goforth@gmail.com>
2022-12-20 21:37:00 -0600

    spa update 12/20

--- server/routers/pizzas.js
+++ server/routers/pizzas.js
@@ -18,7 +18,6 @@ router.get("/", (request, response) => {
     return response.json(record);
   });
 });
-module.exports = router;
 
 // Get a single record by ID using a query parameter
 router.get("/:id", (request, response) => {
@@ -27,3 +26,36 @@ router.get("/:id", (request, response) => {
     return response.json(record);
   });
 });
+
+router.delete("/:id", (request, response) => {
+  Pizza.findByIdAndRemove(request.params.id, {}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.put("/:id", (request, response) => {
+  const body = request.body;
+  Pizza.findByIdAndUpdate(
+    request.params.id,
+    {
+      $set: {
+        // Take note that the customer is not included, so it can't
+        crust: body.crust,
+        cheese: body.cheese,
+        sauce: body.sauce,
+        toppings: body.toppings
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    },
+    (error, record) => {
+      if (error) return response.status(500).json(error);
+      return response.json(record);
+    }
+  );
+});
+
+module.exports = router;
